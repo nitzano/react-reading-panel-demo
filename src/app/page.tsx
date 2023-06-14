fbe18ca3bb3dce5823e4a64ed1abd6cf28f61be8
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2023-06-14 17:50:03 +0300

    panel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,35 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className={styles.main} style={{ display: "flex" }}>
+      <h1>React Reading Panel Demo</h1>
+      <div id="sample">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Eget nunc
+        scelerisque viverra mauris in aliquam. Sit amet commodo nulla facilisi.
+        Id eu nisl nunc mi ipsum faucibus. Arcu non odio euismod lacinia at. Sed
+        arcu non odio euismod lacinia at. Non tellus orci ac auctor augue. Id
+        porta nibh venenatis cras. Cras tincidunt lobortis feugiat vivamus at.
+        Nulla pellentesque dignissim enim sit amet venenatis urna. Faucibus
+        scelerisque eleifend donec pretium vulputate. Potenti nullam ac tortor
+        vitae purus. Tristique sollicitudin nibh sit amet commodo nulla facilisi
+        nullam. Volutpat consequat mauris nunc congue nisi vitae suscipit
+        tellus. Tellus molestie nunc non blandit massa enim nec. Sed turpis
+        tincidunt id aliquet. Non arcu risus quis varius. Placerat duis
+        ultricies lacus sed turpis tincidunt id. Bibendum arcu vitae elementum
+        curabitur. Blandit turpis cursus in hac habitasse platea dictumst
+        quisque sagittis. Urna id volutpat lacus laoreet non curabitur gravida
+        arcu. Eget nullam non nisi est sit. Mi eget mauris pharetra et.
+        Consequat semper viverra nam libero. Sed faucibus turpis in eu mi. Quam
+        quisque id diam vel quam elementum. Egestas maecenas pharetra convallis
+        posuere morbi leo urna molestie at. Massa sed elementum tempus egestas
+        sed sed risus. Dictum varius duis at consectetur lorem. Risus ultricies
+        tristique nulla aliquet enim tortor. Volutpat commodo sed egestas
+        egestas fringilla phasellus. Non diam phasellus vestibulum lorem sed
+        risus ultricies. Leo vel fringilla est ullamcorper. Est sit amet
+        facilisis magna etiam tempor.
       </div>
     </main>
-  )
+  );
 }
